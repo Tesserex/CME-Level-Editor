069152025083451bc1a85d347ebe5c2d2a8d6dc4
Tesserex <tesserex@gmail.com>
2010-07-15 10:36:00 -0500

    new tag based system for handling project tree operations

--- MegaMan Level Editor/ProjectForm.cs
+++ MegaMan Level Editor/ProjectForm.cs
@@ -16,87 +16,22 @@ namespace MegaMan_Level_Editor
         {
             InitializeComponent();
             projectView.NodeMouseDoubleClick += new TreeNodeMouseClickEventHandler(projectView_NodeMouseDoubleClick);
-            projectView.NodeMouseClick += new TreeNodeMouseClickEventHandler(projectView_NodeMouseClick);
-        }
-
-        void projectView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
-        {
-            if (e.Button == MouseButtons.Right)
-                HandleRightClick(e.Node);
         }
 
         void projectView_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            HandleDoubleClick(e.Node);
-        }
-
-        public String StagePath()
-        {
-            return Path.Combine(MainForm.Instance.rootPath, "stages");
-        }
-
-        public String LocalStagePathFor(String stageName)
-        {
-            return Path.Combine("stages", stageName);
+            (e.Node.Tag as ProjectTreeHandler).DoubleClick();
         }
 
         public void AddProject(ProjectEditor project)
         {
             var projectNode = this.projectView.Nodes.Add(project.Name, project.Name);
-            projectNode.Tag = project;
+            projectNode.Tag = new ProjectNodeHandler(projectNode, project);
             foreach (var stage in project.StageNames)
             {
-                projectNode.Nodes.Add(stage, stage);
+                var stagenode = projectNode.Nodes.Add(stage, stage);
+                stagenode.Tag = new StageNodeHandler(stagenode, project, stage);
             }
-        }
-
-        public void HandleDoubleClick(TreeNode node)
-        {
-            if (node.Parent.Name == "stages")
-            {
-                OpenStage(node);
-            }
-            else if (node.Parent.Parent.Name == "stages")
-            {
-                OpenScreenProperties(node);
-            }
-        }
-
-        private void OpenStage(TreeNode node)
-        {
-            MapDocument stage = MainForm.Instance.OpenStage(node.Name, LocalStagePathFor(node.Name));
-            LoadScreenSubtree(node, stage.Screens);
-
-            stage.ScreenAdded += (screen) =>
-                {
-                    var stageNode = projectView.Nodes.Find(screen.Map.Name, true).First();
-                    node.Nodes.Add(screen.Name, screen.Name);
-                };
-
-            stage.ReFocus();
-        }
-
-        private void LoadScreenSubtree(TreeNode node, IEnumerable<ScreenDocument> screens)
-        {
-            node.Nodes.Clear();
-            foreach (var screen in screens)
-            {
-                node.Nodes.Add(screen.Name, screen.Name);
-            }
-        }
-
-        private void OpenScreenProperties(TreeNode node)
-        {
-            var stageName = node.Parent.Name;
-            var screenName = node.Name;
-            var screen = MainForm.GetScreen(stageName, screenName);
-            var screenProps = new ScreenProp(screen);
-            screenProps.Show();
-        }
-
-        public void HandleRightClick(TreeNode node)
-        {
-            
         }
     }
 }
---
+++ MegaMan Level Editor/ProjectTreeHandler.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MegaMan_Level_Editor
+{
+    // all nodes in the project tree should have one of these as its tag
+    public abstract class ProjectTreeHandler
+    {
+        protected TreeNode parentNode;
+
+        public abstract void DoubleClick();
+        public abstract void Properties();
+        public abstract void Delete();
+    }
+
+    public class ProjectNodeHandler : ProjectTreeHandler
+    {
+        private ProjectEditor project;
+
+        public ProjectNodeHandler(TreeNode node, ProjectEditor project)
+        {
+            this.parentNode = node;
+            this.project = project;
+        }
+
+        public override void Properties()
+        {
+            new ProjectProperties(this.project).Show();
+        }
+
+        public override void Delete() { }
+
+        public override void DoubleClick() { }
+    }
+
+    public class StageNodeHandler : ProjectTreeHandler
+    {
+        private ProjectEditor project;
+        private MapDocument stage;
+        private string stageName;
+
+        public StageNodeHandler(TreeNode node, ProjectEditor project, string stageName)
+        {
+            this.parentNode = node;
+            this.project = project;
+            this.stage = null;
+            this.stageName = stageName;
+        }
+
+        public StageNodeHandler(TreeNode node, ProjectEditor project, MapDocument stage)
+        {
+            this.parentNode = node;
+            this.project = project;
+            this.stage = stage;
+            this.stageName = stage.Name;
+        }
+
+        public override void DoubleClick()
+        {
+            if (this.stage == null)
+            {
+                this.stage = this.project.StageByName(stageName);
+                parentNode.Nodes.Clear();
+                foreach (var screen in stage.Screens)
+                {
+                    var node = new TreeNode(screen.Name);
+                    node.Tag = new ScreenNodeHandler(node, screen);
+                    parentNode.Nodes.Add(node);
+                }
+
+                stage.ScreenAdded += (screen) =>
+                {
+                    var node = new TreeNode(screen.Name);
+                    node.Tag = new ScreenNodeHandler(node, screen);
+                    parentNode.Nodes.Add(node);
+                };
+            }
+
+            stage.ReFocus();
+        }
+
+        public override void Delete() { }
+
+        public override void Properties()
+        {
+            var stageprop = new StageProp();
+            stageprop.LoadMap(this.stage);
+            stageprop.Show();
+        }
+    }
+
+    public class ScreenNodeHandler : ProjectTreeHandler
+    {
+        private ScreenDocument screen;
+
+        public ScreenNodeHandler(TreeNode node, ScreenDocument screen)
+        {
+            this.parentNode = node;
+            this.screen = screen;
+        }
+
+        public override void DoubleClick() { }
+
+        public override void Delete() { }
+
+        public override void Properties()
+        {
+            new ScreenProp(this.screen).Show();
+        }
+    }
+}
